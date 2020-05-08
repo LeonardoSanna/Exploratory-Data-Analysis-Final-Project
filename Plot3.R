ce4bd86e32475fcdaf90811bd38ceb7d8a12e553
Leonardo Sanna <leonardo.sanna@unimore.it>
2020-05-09 00:53:24 +0200

    second version

--- Plot3.R
+++ Plot3.R
@@ -30,3 +30,18 @@ ggplot(data = em_types_baltimore,
 
 dev.copy(png, "plot3.png")
 dev.off()
+
+
+#alternative (by another work on coursera)
+baltcitymary.emissions.byyear<-summarise(group_by(filter(NEI, fips == "24510"), year,type), Emissions=sum(Emissions))
+clrs <- c("red", "green", "blue", "yellow")
+ggplot(baltcitymary.emissions.byyear, aes(x=factor(year), y=Emissions, fill=type,label = round(Emissions,2))) +
+        geom_bar(stat="identity") +
+        #geom_bar(position = 'dodge')+
+        facet_grid(. ~ type) +
+        xlab("year") +
+        ylab(expression("total PM"[2.5]*" emission in tons")) +
+        ggtitle(expression("PM"[2.5]*paste(" emissions in Baltimore ",
+                                           "City by various source types", sep="")))+
+        geom_label(aes(fill = type), colour = "white", fontface = "bold")
+png(filename = "plot1.png", width = 1200, height = 793, units = "px")
